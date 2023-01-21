cdfb7e173b0b9f66965eec43b5a190e351b4c97b
FacelessKiller <116458219+FacelessKiller@users.noreply.github.com>
2023-01-20 22:26:33 -0500

    Update bypasscode.js

--- bypasscode.js
+++ bypasscode.js
@@ -1,93 +1,39 @@
-function m() {
-    var r='',c='ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-    for ( var i = 0; i < 5; i++ ) {
-      r += c.charAt(Math.floor(Math.random() * 36));
-   }
-   return r;
+function generateRandomString() {
+    let result = "",
+        characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    for (let i = 0; i < 5; i++) {
+        result += characters.charAt(Math.floor(Math.random() * 36));
+    }
+
+    return result;
 }
 
-if (location.host != "chrome.google.com" || !location.pathname.startsWith("/webstore")) {
-    location.href = "https://chrome.google.com/webstore" + m();
+// Redirect to the Chrome Webstore if not already on it
+if (location.host !== "chrome.google.com" || !location.pathname.startsWith("/webstore")) {
+    location.href = `https://chrome.google.com/webstore/${generateRandomString()}`;
 }
 
-document.head.innerHTML = `
-<style>
-tr:nth-child(even){background-color: #f2f2f2;}
-tr:hover {background-color: #ddd;}
-td, th {
-  border: 1px solid #ddd;
-  padding: 8px;
-  font-family: Arial, Helvetica, sans-serif;
-  border-collapse: collapse;
-}
-.switch {
-  position: relative;
-  display: inline-block;
-  width: 40px;
-  height: 23px;
-}
-.switch input {
-  opacity: 0;
-  width: 0;
-  height: 0;
-}
-.slider {
-  position: absolute;
-  cursor: pointer;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: #ccc;
-  -webkit-transition: .4s;
-  transition: .4s;
-}
-.slider:before {
-  position: absolute;
-  content: "";
-  height: 17px;
-  width: 17px;
-  left: 3px;
-  bottom: 3px;
-  background-color: white;
-  -webkit-transition: .4s;
-  transition: .4s;
-}
-input:checked + .slider {
-  background-color: #2196F3;
-}
-input:focus + .slider {
-  box-shadow: 0 0 1px #2196F3;
-}
-input:checked + .slider:before {
-  -webkit-transform: translateX(17px);
-  -ms-transform: translateX(17px);
-  transform: translateX(17px);
-}
-/* Rounded sliders */
-.slider.round {
-  border-radius: 23px;
-}
-.slider.round:before {
-  border-radius: 50%;
-}
-</style>
-`;
+// Create a table to display information about all installed extensions
 document.body = document.createElement("body");
+document.newBodyData = `<table>`;
 
-document.toggleFunction = function(id){
-    var clickedRow = document.getElementById(id);
-    chrome.management.setEnabled(id, clickedRow.children[0].children[0].children[0].checked);
-};
-
-document.uninstallFunction = function(id){
-    chrome.management.uninstall(id, {showConfirmDialog: true}, function(result){
-        if(result){
-            var row = document.getElementById(id);
-            row.parentNode.removeChild(row);
-        }
+chrome.management.getAll(function (extensions) {
+    extensions.forEach(function (extension) {
+        document.newBodyData += `
+            <tr id=${extension.id}>
+                <td>
+                    <label class='switch'>
+                        <input type='checkbox' ${extension.enabled ? "checked" : ""} onclick="toggleFunction('${extension.id}')">
+                        <span class='slider round'></span>
+                    </label>
+                </td>
+                <td>${extension.name}</td>
+                <td>${extension.id}</td>
+                <td>${extension.installType}</td>
+            </tr>
+        `;
     });
-};
-
-document.newBodyData = "<table>"
-console.
+    document.newBodyData += `</table>`;
+    document.body.innerHTML = document.newBodyData;
+});
